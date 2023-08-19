522cb8213755084a7a0d94c0f3eb684cc805909b
Ofthemasses <43369124+ofthemasses@users.noreply.github.com>
2023-08-19 18:32:00 +1200

    Get file data from response and echo as new response

--- Toru/ApiHandler/SteamApiHandler.php
+++ Toru/ApiHandler/SteamApiHandler.php
@@ -16,14 +16,14 @@ class SteamApiHandler extends ApiHandler
             return "BLOCKED ACTION";
         }
 
-        $steamApiUrl = "https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/";
+        $steamApiCollectionUrl = "https://api.steampowered.com/ISteamRemoteStorage/GetCollectionDetails/v1/";
         $apiKey = $_ENV["STEAM_API_KEY"];
 
         // Set up POST data
         $postData = array(
             "key" => $apiKey,
             "steamid" => $args[0],
-	    "collectioncount" => 1,
+            "collectioncount" => 1,
             "publishedfileids[0]" => $args[1]
         );
 
@@ -39,8 +39,39 @@ class SteamApiHandler extends ApiHandler
 
         // Create context and send the POST request
         $context = stream_context_create($options);
-        $response = file_get_contents($steamApiUrl, false, $context);
+        $response = file_get_contents($steamApiCollectionUrl, false, $context);
 
-        return $response;
+        $decodedResponse = json_decode($response, true);
+
+        if (isset($decodedResponse['response']['result']) && $decodedResponse['response']['result'] == 1) {
+            $steamApiFileUrl = "https://api.steampowered.com/ISteamRemoteStorage/GetPublishedFileDetails/v1/";
+            $children = $decodedResponse['response']['collectiondetails'][0]['children'];
+
+            // Set up POST data
+            $postData = array(
+                "key" => $apiKey,
+                "steamid" => args[0],
+                "itemcount" => count($children)
+            );
+
+            $i = 0;
+            // Add each file id
+            foreach ($children as $child) {
+                $postData["publishedfileids[$i]"] = $child['publishedfileid'];
+            }
+
+            // Set up options for the POST request
+            $options = array(
+                'http' => array(
+                    'method' => 'POST',
+                    'header' => "Content-type: application/x-www-form-urlencoded\r\n",
+                    'content' => http_build_query($postData)
+                )
+            );
+
+            $context = stream_context_create($options);
+            return file_get_contents($steamApiCollectionUrl, false, $context);
+        }
+        return "";
     }
 }
