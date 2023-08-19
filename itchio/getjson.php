bb8417758754d7c2914bd51df190f6e3d9b2ec1c
Finlay Metcalfe <43369124+Ofthemasses@users.noreply.github.com>
2023-08-19 18:37:35 +1200

    Revert "4 create itchio api handler"

--- itchio/getjson.php
+++
@@ -1,9 +0,0 @@
-<?php
-require_once('../Toru/ApiHandler/ItchioApiHandler.php');
-require_once('../config.php');
-
-use \Toru\ApiHandler\ItchioApiHandler;
-$handler = new ItchioApiHandler();
-
-header("Content-Type: application/json");
-echo $handler->printResponse([]);
