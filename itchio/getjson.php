5ed4ec5d132fa4a60c934db086838b5c24c08fce
Ofthemasses <43369124+ofthemasses@users.noreply.github.com>
2023-08-19 18:56:30 +1200

    Fixed up dependency issue in itchio page, removed unnecessary arguments.

--- itchio/getjson.php
+++ itchio/getjson.php
@@ -1,9 +1,9 @@
 <?php
-require_once('../Toru/ApiHandler/SteamApiHandler.php');
+require_once('../Toru/ApiHandler/ItchioApiHandler.php');
 require_once('../config.php');
 
 use \Toru\ApiHandler\ItchioApiHandler;
 $handler = new ItchioApiHandler();
 
 header("Content-Type: application/json");
-echo $handler->printResponse([$_GET["user"], $_GET["collection"]]);
+echo $handler->printResponse([]);
